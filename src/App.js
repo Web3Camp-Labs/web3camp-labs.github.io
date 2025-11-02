ebc4b204b8624925041654131c3191f7202416c2
wendychaung <7518647+wendychaung@users.noreply.github.com>
2025-11-03 00:13:40 +0800

    Revert "fix: update router"
    This reverts commit 6e5387ec4acbbad7b18cb5bfbed4b17f7057499a.

--- src/App.js
+++ src/App.js
@@ -2,45 +2,22 @@ import GlobalStyle from "./utils/GloablStyle";
 import { WOW } from 'wowjs';
 import "animate.css";
 import {useEffect} from "react";
-import { BrowserRouter as Router, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import RouterLink from "./router/router";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
-function AppContent() {
-    const navigate = useNavigate();
-    
+function App() {
     useEffect(() => {
         new WOW().init();
-        
-        // 只在生产环境处理路由参数
-        if (window.location.hostname === 'web3camp.us') {
-            const urlParams = new URLSearchParams(window.location.search);
-            const redirectRoute = urlParams.get('route');
-            const validRoutes = ['/home', '/tutorial', '/tools', '/tasks', '/news'];
-            
-            // 如果有路由参数，导航到对应页面并清理URL
-            if (redirectRoute && validRoutes.includes(redirectRoute)) {
-                navigate(redirectRoute);
-                window.history.replaceState({}, '', redirectRoute);
-            }
-        }
-    }, [navigate]);
-    
-    return (
-        <>
+    }, []);
+  return (
+    <div>
+
+        <Router>
             <Header />
             <RouterLink />
             <Footer />
-        </>
-    );
-}
-
-function App() {
-  return (
-    <div>
-        <Router>
-            <AppContent />
         </Router>
       <GlobalStyle />
     </div>
